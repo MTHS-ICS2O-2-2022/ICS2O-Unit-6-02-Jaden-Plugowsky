aa0b312cf94ce84cf400d913c92c32b6920f39cf
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-23 17:32:40 +0000

    initial website

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
-function buttonOneClicked() {
-  alert("Hello, World!");
-  document.getElementById("answer").innerHTML =
-    "The answer is: " + "insert answer here";
+function websiteLoaded() {
+  if (localStorage.clicks) {
+    document.getElementById("answer").innerHTML = localStorage.clicks
+  } else {
+    localStorage.clicks = 1
+  }
+}
+
+function cookieClicked() {
+  localStorage.clicks = Number(localStorage.clicks) + 1
+  document.getElementById("answer").innerHTML = localStorage.clicks
 }
