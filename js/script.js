ce0d553a1b916fa733f46cd9257e9703aa5f3028
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-06-09 02:36:28 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 // Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
 // Created by: Jaden Plugowsky
-// Created on: March 2023
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
 function websiteLoaded() {
+  // This function runs when the website loads, checking for local storage or creating it for the first time
   if (localStorage.clicks) {
     document.getElementById("answer").innerHTML = localStorage.clicks
   } else {
-    localStorage.clicks = 1
+    localStorage.clicks = 0
+    document.getElementById("answer").innerHTML = localStorage.clicks
   }
 }
 
 function cookieClicked() {
+  // This function runs when the cookie is clicked, adding one to the counter, as well as updating the text
   localStorage.clicks = Number(localStorage.clicks) + 1
+  document.getElementById("answer").innerHTML = localStorage.clicks
+}
+
+function resetClicks() {
+  // This function runs when the "Reset Clicks" button is pressed, setting the counter to zero
+  localStorage.clicks = 0
   document.getElementById("answer").innerHTML = localStorage.clicks
 }
